6eb5ae71d8faaf0913da02525e554fe15e95f35b
Arnaud Taffanel <arnaud@bitcraze.io>
2023-01-01 11:24:42 +0100

    Day 16: Part 1: Better, faster, solution
    This should be a good starting point for Part 2!

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,8 +1,9 @@
-
-use std::{collections::BTreeSet, fs, str::FromStr};
 use regex::Regex;
-
-use rayon::prelude::*;
+use std::{
+    collections::{BinaryHeap, VecDeque},
+    fs,
+    str::FromStr,
+};
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Valve {
@@ -36,20 +37,86 @@ impl FromStr for Valve {
 #[derive(Debug, Clone)]
 struct Cave {
     valves: Vec<Valve>,
+    distances: Vec<Vec<usize>>, // Matrix of distances[from][to] valves
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone)]
 struct State {
     valves_opened: Vec<bool>,
+    current_valve: usize,
     released: usize,
-    current_room: usize,
+    time: usize,
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.score() == other.score()
+    }
+}
+
+impl Eq for State {}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.score().partial_cmp(&other.score())
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score().total_cmp(&other.score())
+    }
+}
+
+impl State {
+    fn score(&self) -> f64 {
+        self.released as f64 / self.time as f64
+    }
+
+    fn travel(&mut self, to_valve: usize, travel_time: usize, valves: &Vec<Valve>) {
+        self.released += self.release_rate(valves) * (travel_time + 1);
+        self.current_valve = to_valve;
+        self.time += travel_time + 1; // We need 1 minute to open the valve!
+        self.valves_opened[to_valve] = true;
+    }
+
+    fn release_rate(&self, valves: &[Valve]) -> usize {
+        valves
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| self.valves_opened[*i])
+            .map(|(_, v)| v.rate)
+            .sum()
+    }
+}
+
+fn find_valve_distances(from: usize, valves: &Vec<Valve>) -> Vec<usize> {
+    let mut distances = vec![0; valves.len()];
+    let mut to_visit = VecDeque::new();
+
+    to_visit.push_front(from);
+
+    while !to_visit.is_empty() {
+        let valve_id = to_visit.pop_back().unwrap();
+
+        for next in valves[valve_id].next.iter() {
+            let next_id = valves.iter().position(|v| &v.name == next).unwrap();
+
+            if next_id != from && distances[next_id] == 0 {
+                distances[next_id] = distances[valve_id] + 1;
+                to_visit.push_front(next_id);
+            }
+        }
+    }
+
+    distances
 }
 
 impl FromStr for Cave {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let valves = s
+        let valves: Vec<Valve> = s
             .lines()
             .map(|line| {
                 let valve: Valve = line.parse().unwrap();
@@ -57,82 +124,76 @@ impl FromStr for Cave {
             })
             .collect();
 
-        Ok(Cave { valves })
+        let distances = (0..valves.len())
+            .map(|v| find_valve_distances(v, &valves))
+            .collect();
+
+        Ok(Cave { valves, distances })
     }
 }
 
 impl Cave {
-    fn find_best_route(&self) -> usize {
-        let aa_room = self.valves.iter().position(|valve| valve.name == "AA").unwrap();
+    fn find_max_release(&self, run_time: usize) -> usize {
+        let aa_valve = self.valves.iter().position(|v| &v.name == "AA").unwrap();
 
-        let mut states: BTreeSet<_> = [State {
+        let mut states: BinaryHeap<State> = [State {
             valves_opened: vec![false; self.valves.len()],
+            current_valve: aa_valve,
             released: 0,
-            current_room: aa_room,
-        }].iter().cloned().collect();
+            time: 0,
+        }]
+        .iter()
+        .cloned()
+        .collect();
 
-        for time in 0..30 {
-            println!("Simulating time {}, {} states", time, states.len());
+        let mut max_release = 0;
+        let mut max_time = 0;
 
-            let new_states = states.par_iter().cloned().map(|mut state| {
-                let mut new_states = Vec::new();
-                
-                state.released += state
-                    .valves_opened
-                    .iter()
-                    .enumerate()
-                    .filter(|(_, opened)| **opened)
-                    .map(|(i, _)| self.valves[i].rate)
-                    .sum::<usize>();
+        while !states.is_empty() {
+            states = states
+                .iter()
+                .flat_map(|state| {
+                    // update max_release with this state at run time
+                    let this_max = ((run_time - state.time) * state.release_rate(&self.valves))
+                        + state.released;
+                    max_release = max_release.max(this_max);
+                    max_time = max_time.max(state.time);
 
-                // If all valves are already opened, do nothing
-                if state.valves_opened.iter().all(|opened| *opened) {
-                    new_states.push(state.clone());
-                    return new_states;
-                }
+                    // Visit all possible valves as long as we do not go over the runtime
+                    state
+                        .valves_opened
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, opened)| !*opened)
+                        .map(|(valve_id, _)| {
+                            let mut state = state.clone();
 
-                // Open the valve
-                if !state.valves_opened[state.current_room] {
-                    let mut new_state = state.clone();
-                    new_state.valves_opened[state.current_room] = true;
-                    new_states.push(new_state);
-                }
+                            let travel_time = self.distances[state.current_valve][valve_id];
+                            state.travel(valve_id, travel_time, &self.valves);
 
-                // Move to all possible rooms
-                for room in &self.valves[state.current_room].next {
-                    let mut new_state = state.clone();
-                    let new_room = self.valves.iter().position(|valve| valve.name == room.to_owned()).unwrap();
-                    new_state.current_room = new_room;
-                    new_states.push(new_state);
-                }
+                            state
+                        })
+                        .filter(|state| state.time < run_time)
+                        .filter(|state| self.valves[state.current_valve].rate > 0)
+                })
+                .collect();
 
-                new_states
-            }).flatten().collect();
-
-            states = new_states;
-
-            // Prune state
-            let max_released = states.iter().map(|state| state.released).max().unwrap();
-
-            if states.len() > 10000000 {
-                dbg!(states.len());
-                dbg!(states.iter().map(|state| state.released).max());
-                states.retain(|state| state.released as f64 >= max_released as f64 * 0.9f64);
-                dbg!(states.len());
-                dbg!(states.iter().map(|state| state.released).max());
-            }
+            println!(
+                "{} states, max release: {}, max_time: {}",
+                states.len(),
+                max_release,
+                max_time
+            );
         }
 
-        let max_released = states.iter().map(|state| state.released).max().unwrap();
-
-        max_released
+        max_release
     }
 }
 
 fn main() -> anyhow::Result<()> {
     let cave: Cave = fs::read_to_string("input").unwrap().parse()?;
 
-    println!("Max release: {}", cave.find_best_route());
+    println!("Max release: {}", cave.find_max_release(30));
 
     Ok(())
 }
